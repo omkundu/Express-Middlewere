ec2361f328f2e4355661db1760d1c22091ab5e79
omkundu <omkundu156@gmail.com>
2023-04-14 14:00:40 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-hey
+const express=require("express");
+const app =express();
+
+app.use(()=>{
+    console.log("Hello from Middlewere");
+});
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome")
+})
+
+app.get("/contacts",(req,res)=>{
+    res.send("Contacts")
+})
+
+
+app.get("/about",(req,res)=>{
+    res.send("About")
+})
+
+app.get("/blogs",(req,res)=>{
+    res.send("Blogs")
+})
+
+
+
+
+
+
+
+app.listen(7640,()=>{
+    console.log("Listening on port 7640")
+})
